42daa97579ab034955f28352cc0f0863c17e0867
Emmanuel Oluwafemi <fasogbaemmanuel@gmail.com>
2024-12-04 01:42:10 +0100

    Add failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn Jenkins link', () => {
+test("renders learn Jenkins link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn Jenkins/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders my name", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Emmanuel/i);
   expect(linkElement).toBeInTheDocument();
 });
